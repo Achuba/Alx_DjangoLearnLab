5dd09f428b5f486c9fa9ace12a782508de4e90db
Achuba <achubadaniel45@gmail.com>
2026-01-10 17:44:16 +0100

    change the import on the admin.py file

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Introduction_to_Django.LibraryProject.bookshelf.models import Book
+from .models import Book
 
 # Register your models here.
 admin.site.register(Book)
