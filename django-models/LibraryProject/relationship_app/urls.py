e1ebb26cfafa6793f4db0a93a542756a59d432d3
Achuba <achubadaniel45@gmail.com>
2026-01-19 00:55:17 +0100

    impt list_books from .views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from relationship_app.views import book_list_view, LibraryDetailView, list_books
+from .views import list_books
 
 urlpatterns = [
     path("", include("relationship_app.urls")),
