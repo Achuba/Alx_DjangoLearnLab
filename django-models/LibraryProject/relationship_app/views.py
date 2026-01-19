3bdb9d8a1969953b09645c68c60acd4af2d4561a
Achuba <achubadaniel45@gmail.com>
2026-01-19 01:48:41 +0100

    Added view.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .query_samples import query_books_by_author, list_books_in_library, get_librarian_of_library
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
@@ -43,3 +44,28 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == "Admin"
+
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
+
