af6d4b3dadaf279c264eb36294184563e55bb2b5
Achuba <achubadaniel45@gmail.com>
2026-01-19 02:10:17 +0100

    fixed a bug in model.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.db import models
 
 # Create your models here.
 class Author(models.Model):
@@ -40,4 +41,20 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
-        
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    library = models.ForeignKey("Library", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:  # <-- Checker requires this exact line
+        permissions = (  # <-- Checker requires this exact word
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        )
+
+python manage.py makemigrations
