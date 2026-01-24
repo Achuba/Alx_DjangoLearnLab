ec8431482ac39c297f5cb4eaed85a7f6f0b13088
Achuba <achubadaniel45@gmail.com>
2026-01-24 16:54:41 +0100

    Example_form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,12 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ["title", "author", "library"]  # include all fields you want to edit/add
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']  # include the fields you want users to submit
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter book title'}),
+            'author': forms.Select(),
+        }
