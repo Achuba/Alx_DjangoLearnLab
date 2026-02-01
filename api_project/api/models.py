c13bb243bab8b4585e16d2716e7f21ef52472bb2
Achuba <achubadaniel45@gmail.com>
2026-02-01 18:33:36 +0100

    Added api

---
+++ api_project/api/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    join_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Loan(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    loan_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.book.title} loaned to {self.member.name}"
+    
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
